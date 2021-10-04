14604af8424ab883f43d981538140404cbdb68d0
vi2920va <vi2920va@gmail.com>
2021-10-04 14:53:23 +0900

    Add: Button 컴포넌트 추가

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Button.scss';
+
+const Button = ({ type = 'button', children, name, onClick }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    onClick(isClicked);
+  };
+
+  return (
+    <button
+      type={type}
+      className={`button button--${name}`}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
